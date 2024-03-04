cb263b25ff115817e8a11982828f0f72ff6c2cf8
naser66 <zagros.k66@gmail.com>
2024-03-05 02:04:39 +0330

    add BookDeleteView to BookStore

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('<int:pk>/', BookDetailView.as_view(), name='book_detail'),
     path('create/', BookCreateView.as_view(), name='create_book'),
-    path('<int:pk>/edit/', BookUpdateView.as_view(), name='bood_edit'),
+    path('<int:pk>/edit/', BookUpdateView.as_view(), name='book_edit'),
+    path('<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
 ]
