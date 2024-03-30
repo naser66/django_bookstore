bd94087c98ed5b59dbf19b1e142c5424f9056490
naser66 <zagros.k66@gmail.com>
2024-03-30 21:02:56 +0330

    change home page

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from books.views import BookListView
 
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', BookListView.as_view(), name='home'),
 ]
